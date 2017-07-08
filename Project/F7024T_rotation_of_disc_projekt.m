becd85b296c61c11e468a68faf86e9020f1c7d1f
Daniel Brolin <daniel-brolin@hotmail.com>
2017-07-08 23:34:44 +0200

    Report 3 complete
    If anything looks weird please feel free to text me!

--- Project/F7024T_rotation_of_disc_projekt.m
+++ Project/F7024T_rotation_of_disc_projekt.m
@@ -3,18 +3,32 @@ clear
 clc
 
 syms u E omega v rho f_r
-R_i=1;                     %inner radii
-R_o=2;                     %outer radii
-r=linspace(R_i, R_o);      %radius
-omega= 1;                  %angular velocity, rad/s
+constex1=1; %acts as placeholders
+constex2=2; %
 
-
+R_i=constex1;                       %inner radii
+R_o=constex2;                       %outer radii
+r=linspace(R_i,R_o);                %radius
+v = constex1;                       %velocity
+omega= constex1;                    %angular velocity, rad/s
 
 %u=                     %Nodal value
 %E=                     %elastic modulus
 %v=                     %poissons ratio
 %rho=                   %density
 
-%f_r=rho*r*omega^2      %volumetric loading for rotating disc
+A = (3+v)/8*rho*omega^2*(Ri^2*Ro^2-Ri^4)/Ri^2;
+B = (3+v)/8*rho*omega^2*Ri^2*Ro^2;
 
+%eps_r = diff(u);
+%eps_phi = u/r;
+%S_r = (E/(1-v^2))*[1 v]*[ers_r;eps_phi]
+%S_phi = (E/(1-v^2))*[v 1]*[ers_r;eps_phi]
+
+%f_r=rho*r*omega^2      %volumetric loading for rotating disc
 %L = @(u)diff(u,r,2)+diff(u,r)*./r-u/r^2+(1-v^2)*f_r/E;
+
+%--------------------------------------------------
+% Galerkin and Ritz
+
+
